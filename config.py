f5b4926e9a081c35f9179dc1647718c96bc9025d
DeveloperSubhadeep1 <161279108+DeveloperSubhadeep1@users.noreply.github.com>
2024-04-25 22:22:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,8 +27,8 @@ class Config(object):
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
     PORT = int(os.environ.get("PORT", "8080"))
-    PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "120"))
-    PING_WEB   = os.environ.get("PING_WEB", "120")
+    PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "1200"))
+    PING_WEB   = os.environ.get("PING_WEB", "")
 
 class Txt(object):
     # part of text configuration
